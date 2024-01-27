b3b26add3e7dc34c17bfed550332c799f51a3dce
mmpotulo28 <mpotulom28@gmail.com>
2024-01-27 15:16:45 +0200

    Add loader pop-up and form validation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,8 @@
-// import emailjs from 'emailjs-com';
-
 const contactForm = document.querySelector( '#contacts-form' );
 const contactName = document.querySelector( '#contact-name' );
 const contactEmail = document.querySelector( '#contact-email' );
 const contactMessage = document.querySelector( '#contact-message' );
+const loader = document.querySelector( '.loader-pop-up' );
 
 contactForm.addEventListener( 'submit', ( event ) => {
     event.preventDefault();
@@ -14,37 +13,51 @@ contactForm.addEventListener( 'submit', ( event ) => {
         message: contactMessage.value
     };
 
-    validateInput( formData.name, formData.email );
-    sendEmail( formData );
+    validateInput( formData.name, formData.email, formData );
 } );
 
 const emailError = document.querySelector( '#email-error' );
 const nameError = document.querySelector( '#name-error' );
 
-function validateInput ( name, email ) {
+async function validateInput ( name, email, formData ) {
     let nameArr = name.split( ' ' );
     name = nameArr.join( '' ).trim();
     email = email.trim();
 
-    if ( !/^[a-zA-Z]+$/.test( name ) ) {
-        nameError.style.display = 'block';
-        nameError.textContent = 'Please enter a valid name';
-    } else {
-        nameError.style.display = 'none';
-        nameError.textContent = '';
+    try {
+        await new Promise( ( resolve, reject ) => {
+            if ( !/^[a-zA-Z]+$/.test( name ) ) {
+                nameError.style.display = 'block';
+                nameError.textContent = 'Please enter a valid name';
+            } else {
+                nameError.style.display = 'none';
+                nameError.textContent = '';
+            }
+
+            if ( !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test( email ) ) {
+                emailError.style.display = 'block';
+                emailError.textContent = 'Please enter a valid email';
+            } else {
+                emailError.style.display = 'none';
+                emailError.textContent = '';
+            }
+
+            if ( nameError.style.display === 'none' && emailError.style.display === 'none' ) {
+                resolve();
+            } else {
+                reject('form validation error');
+            }
+        } );
+    } catch (error) {
+            throw new Error( error );
     }
 
-    if ( !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test( email ) ) {
-        emailError.style.display = 'block';
-        emailError.textContent = 'Please enter a valid email';
-    } else {
-        emailError.style.display = 'none';
-        emailError.textContent = '';
-    }
+    sendEmail( formData );
 }
 
 async function sendEmail ( formData ) {
     try {
+        loader.style.display = 'flex';
         await emailjs.send( 'service_5fplxzq', 'template_59ytvcn', {
             from_name: formData.name,
             to_name: 'Manelisi Mpotulo',
@@ -52,12 +65,13 @@ async function sendEmail ( formData ) {
             reply_to: formData.email,
         }, 'o24mx-l8WjlWOujcE' );
 
+        loader.style.display = 'none';
         alert( 'Form submitted successfully, Thank you for your message' );
 
         contactName.value = '';
         contactEmail.value = '';
         contactMessage.value = '';
-        
+
     } catch ( error ) {
         alert( 'Something went wrong, please try again later' );
         throw new Error( error );
